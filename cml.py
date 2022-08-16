8c1752ce506571bc08521934ef6a69bba2ab7b9e
ctvedt <hei@ctvedt.no>
2022-08-16 19:36:26 +0200

    added code for sending email via sendgrid

--- cml.py
+++ cml.py
@@ -5,10 +5,20 @@ from decouple import config
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 # Get environment variables
 base_url = config('CML_API_BASE_URL')
 username = config('CML_USERNAME')
 password = config('CML_PASSWORD')
+sendgrid_api_key = config('SENDGRID_API_KEY')
+sendgrid_from_email = config('SENDGRID_FROM_EMAIL')
+download_base_url = config('DOWNLOAD_BASE_URL')
+
+# email for testing
+email = config('DEBUG_EMAIL')
 
 def GetToken(username, password):
     api_url = "authenticate"
@@ -46,8 +56,11 @@ def DownloadLab(token, labId):
     return r.text
 
 def SaveLab(labId, labFile):
-    with open(f'lab_{labId}.yaml', 'w') as file:
-        file.write(f'./labs/{labFile}')
+    labs_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'labs/')
+    if not os.path.isdir(labs_directory):
+        os.mkdir(labs_directory)
+    with open(f'{os.path.join(labs_directory, labId)}.yaml', 'w') as file:
+        file.write(labFile)
 
 def StopLab(token, labId):
     api_url = f"labs/{labId}/stop"
@@ -93,11 +106,17 @@ def SaveTempPassword(password):
     with open(f'temp_password', 'w') as file:
         file.write(password)
 
-def SendUsernameAndPassword(email, password):
-    pass
-
-def SendConfigs(email):
-    pass
+def SendEmail(email, title, content):
+    message = Mail(
+        from_email=sendgrid_from_email,
+        to_emails=email,
+        subject=title,
+        html_content=content)
+    try:
+        sg = SendGridAPIClient(sendgrid_api_key)
+        response = sg.send(message)
+    except Exception as e:
+        print(e.message)
 
 # Generate random password and save it to file
 temp_password = uuid.uuid4().hex
@@ -108,10 +127,12 @@ def CleanUp():
     token = GetToken(username, temp_password)
     labs = GetListOfAllLabs(token)
     # Loop through all labs, save config, stop and delete labs
+    userlabs = []
     for lab in labs:
         nodes = GetNodesInLab(token, lab)
         for node in nodes:
             SaveNodeConfig(token, lab,node)
+        userlabs.append(lab)
         SaveLab(lab, DownloadLab(token, lab))
         StopLab(token, lab)
         DeleteLab(token, lab)
@@ -119,10 +140,21 @@ def CleanUp():
     UpdateUserPassword(token, GetAdminId(token), temp_password, password)
     # Re-authenticate and log out all users
     LogAllUsersOut(GetToken(username, password))
+    # Email the user with download URL to their labs, if any
+    if userlabs:
+        lablinks = "Download links for your lab(s):<br>"
+        for lab in userlabs:
+            lablinks += f" - {download_base_url}" + lab +".yaml <br>"
+        SendEmail(email, 'CML labs', f"Hi!<br><br>Your booked timeslot has unfortunatly come to an end, but don't worry!<br>All your labs are saved and can to be downloaded easily. <br><br>{lablinks}")
 
 def CreateTempUser():
+    # Create temporary password
     token = GetToken(username, password)
     UpdateUserPassword(token, GetAdminId(token), password, temp_password)
+
+    # Send email to the user with the login information
+    SendEmail(email, 'CML login info', f'Use these login credentials:<br><br>Username: {username}<br>Password: {temp_password}')
+
     print("...sleeping for 30s...")
     sleep(30)
     CleanUp()
