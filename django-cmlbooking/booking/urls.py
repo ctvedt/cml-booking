b4270ac14f4ee421a73851ead818e50f62744342
ctvedt <hei@ctvedt.no>
2022-08-21 17:30:47 +0200

    redirect booking to index if no data provided

--- django-cmlbooking/booking/urls.py
+++ django-cmlbooking/booking/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
     path('', views.RenderCalendar, name='index'),
-    path('booking/', views.CreateNewBooking, name='booking'),
-    path('booking/<int:day>/', views.CreateNewBooking, name='booking'),
-    path('booking/<int:day>/<int:slot>/', views.CreateNewBooking, name='booking'),
+    path('booking/', RedirectView.as_view(url='/')),
+    path('booking/<int:day>/', views.CreateNewBooking),
+    path('booking/<int:day>/<int:slot>/', views.CreateNewBooking),
 ]
