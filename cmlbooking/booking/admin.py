c9331e1d0e4917504a4d91f67e454a72fb9cfb32
ctvedt <hei@ctvedt.no>
2022-08-21 12:53:51 +0200

    show password on admin page

--- cmlbooking/booking/admin.py
+++ cmlbooking/booking/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Booking
 
 class BookingAdmin(admin.ModelAdmin):
-    fields = ['timeslot', 'email']
+    fields = ['timeslot', 'email', 'password']
 
 admin.site.register(Booking, BookingAdmin)
