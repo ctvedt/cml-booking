ffa9280e355e7bd4b146c5f0e168e419bd15bd42
Christopher W. Tvedt <hei@ctvedt.no>
2022-08-29 21:20:43 +0200

    Booking cancellation (#8)
    * cancelcode in booking, button in email +++, but missing delete logic
    
    * added cancel logic + migration
    
    * default password and cancelcode was not working
    
    * fixed blue background on cancel button
    
    * squash migrations

--- django-cmlbooking/booking/models.py
+++ django-cmlbooking/booking/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+import uuid
+
+def random_uuid():
+    random_uuid = uuid.uuid4().hex
+    return random_uuid
 
 class Booking(models.Model):
     timeslot = models.DateTimeField()
     email = models.EmailField(blank=False)
-    password = models.TextField(default='NO_PASSWORD_SET')
+    password = models.CharField(max_length=50, blank=True, null=True, editable=True)
+    cancelcode = models.CharField(max_length=50, blank=True, null=True, editable=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.timeslot} - {self.email}'
+
+    def save(self, *args, **kwargs):
+        self.password = random_uuid()
+        self.cancelcode = random_uuid()
+        super(Booking, self).save(*args, **kwargs)
