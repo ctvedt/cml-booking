0893eed40943bffde97494c3c97f3e0671e198a4
ctvedt <hei@ctvedt.no>
2022-08-29 20:58:18 +0200

    default password and cancelcode was not working

--- django-cmlbooking/booking/models.py
+++ django-cmlbooking/booking/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 import uuid
 
+def random_uuid():
+    random_uuid = uuid.uuid4().hex
+    return random_uuid
+
 class Booking(models.Model):
     timeslot = models.DateTimeField()
     email = models.EmailField(blank=False)
-    password = models.CharField(max_length=50, default=uuid.uuid4().hex)
-    cancelcode = models.CharField(max_length=50, default=uuid.uuid4().hex)
+    password = models.CharField(max_length=50, blank=True, null=True, editable=True)
+    cancelcode = models.CharField(max_length=50, blank=True, null=True, editable=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f'{self.timeslot} - {self.email}'
+
+    def save(self, *args, **kwargs):
+        self.password = random_uuid()
+        self.cancelcode = random_uuid()
+        super(Booking, self).save(*args, **kwargs)
